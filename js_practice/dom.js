b3c4f6fb6631619d87fc18d20300935888ddfa46
Frank120 <Frank.Wang@mullenloweprofero.com>
2016-11-09 18:32:47 +0800

    backbone + requireJs

--- js_practice/dom.js
+++ js_practice/dom.js
@@ -35,3 +35,66 @@
 // 	child = child.nextElementChild;
 // }
 
+
+
+// function initUI(){
+// 	with(document){
+// 		var bd    = body,
+// 			links = getElementsByTagName('a'),
+// 			i     = 0,
+// 			len   = links.length;
+// 		while( i < len){
+// 			update(links[i++]);
+// 		}
+// 		getElementById("id").onclick = function(){
+// 			doSomething();
+// 		};
+// 	}
+// }
+
+
+//closure
+// function f1(){
+// 	var n = 999;
+
+// 	nAdd = function(){n += 1};
+
+// 	function f2(){
+// 		alert(n);
+// 	}
+// 	return f2;
+// }
+
+// var result = f1();
+// result();
+// nAdd();
+// result();
+
+// var name = "the window";
+
+// var object = {
+// 	name : "my object",
+// 	getNameFunction :  function(){
+// 		return function(){
+// 			return this.name;
+// 		};
+// 	}
+// };
+
+// alert(object.getNameFunction()());
+
+
+// var name = "the window";
+
+// var object = {
+// 	name : "my object",
+// 	getNameFunction : function(){
+// 		var that = this;
+
+// 		return function(){
+// 			return that.name;
+// 		};
+// 	}
+// };
+
+// alert(object.getNameFunction()());
